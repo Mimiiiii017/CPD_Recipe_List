2dec2f7bc301eb2282b89b7f438e9476e34d0e73
Mireya <vellamireya@gmail.com>
2025-03-20 12:16:23 +0100

    added header with back button for add_recipe_screen.dart

--- lib/screens/add_recipe_screen.dart
+++ lib/screens/add_recipe_screen.dart
@@ -59,7 +59,36 @@ class _AddRecipeScreenState extends State<AddRecipeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
       backgroundColor: const Color(0xFFD9E7E8), 
+
+        appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(60),
+        child: Container(
+          decoration: const BoxDecoration(
+            color: Color(0xFF2F696B),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black12,
+                blurRadius: 5,
+                offset: Offset(0, 3),
+              ),
+            ],
+          ),
+          child: AppBar(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            leading: IconButton(
+              icon: const Icon(Icons.arrow_back, color: Colors.white),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            title: const Text('Add Recipe Form', style: TextStyle(color: Colors.white)),
+          ),
+        ),
+      ),
+
       body: SafeArea(
         child: SingleChildScrollView(
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
